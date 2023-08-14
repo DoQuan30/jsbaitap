d9ee59d6894942dd3f4a67b007fe5494552fbc48
DoQuan30 <bobinchamnet@gmail.com>
2023-08-14 22:08:23 +0700

    thuchanh2

--- baitap1.js
+++ baitap1.js
@@ -1,3 +1,4 @@
-function showMessage(){
-    alert('Xin chào!');
-}
+let rộng = parseInt (prompt("Nhập vào chiều rộng "));
+let cao = parseInt (prompt("Nhập vào chiều cao"));
+let diệntích = rộng * cao;
+document.write ("diện tích = " + diệntích)
